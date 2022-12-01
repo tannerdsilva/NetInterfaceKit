f7d46dfaffcfffb15ba77fccc353150341637c23
Tanner Silva <t@tannersilva.com>
2022-12-01 00:28:30 +0000

    renaming stuff

--- Sources/NetInterfaceKit/NetInterfaceKit.swift
+++ Sources/NetInterfaceKit/NetInterfaceKit.swift
@@ -8,24 +8,25 @@ struct NetInterfaceKit {
 		case noInterfacesFound
 	}
 	
-	struct NetworkInterface:Hashable {
-		let index:UInt32
+	struct InterfaceNameIndex:Hashable {
 		let name:String
+		let index:UInt32
 	}
 	
-	static func allInterfaces() throws -> Set<NetworkInterface> {
+	static func allInterfaces() throws -> Set<InterfaceNameIndex> {
 		guard let getNameArray = if_nameindex() else {
+			Self.logger.error("allInterfaces() called but no network interfaces found")
 			throw Error.noInterfacesFound
 		}
 		defer {
 			if_freenameindex(getNameArray)
 		}
 		var i = getNameArray;
-		var buildResults = Set<NetworkInterface>()
+		var buildResults = Set<InterfaceNameIndex>()
 		while (i.pointee.if_index != 0 && i.pointee.if_name != nil) {
 			let nameString = String(cString:i.pointee.if_name)
 			Self.logger.info("found new interface", metadata:["name":"\(nameString)"])
-			buildResults.update(with:NetworkInterface(index:i.pointee.if_index, name:nameString))
+			buildResults.update(with:InterfaceNameIndex(name:nameString, index:i.pointee.if_index))
 			i = i.advanced(by:1)
 		}
 		return buildResults
